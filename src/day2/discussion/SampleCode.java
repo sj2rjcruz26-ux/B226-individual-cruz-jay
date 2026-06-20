1582935a81b19e66e0b3548a3d35f2160f304b32
Ronald Jay Cruz <sj2.rjcruz26@joysistvi.edu.ph>
2026-06-20 15:30:12 +0800

    Add discussion code for day 2 and day 3

---
+++ src/day2/discussion/SampleCode.java
@@ -0,0 +1,18 @@
+
+package day2.discussion; // package
+
+
+public class SampleCode { // class
+    public static void main(String[] args) { // Main Method
+        System.out.println("Hello My Love Airadale"); // Print Statement
+    }
+}
+
+// Main Method (psvm) -> Starting point of program with excution
+// Print Statement (sout) -> It tells the computer to print something
+
+/*
+    2 Types of Comments
+    1. Single Line Comment
+    2. Multi Line Comment
+*/
---
+++ src/day3/discussion/SampleCode.java
@@ -0,0 +1,19 @@
+
+package day3.discussion; // package
+
+
+public class SampleCode { // class
+    public static void main(String[] args) { // Main Method
+        System.out.println("Hello My Love"); // Print Statement
+    }
+}
+// Syntax -> structure pf code
+// Syntax Rule -> Syntax Error
+// Main Method (psvm + tab) -> Starting point of program with excution
+// Print Statement (sout + tab) -> It tells the computer to print something
+
+/*
+    2 Types of Comments
+    1. Single Line Comment
+    2. Multi Line Comment
+*/
